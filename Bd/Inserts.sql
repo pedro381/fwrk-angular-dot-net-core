20abf5099fa4eb4c2be8f278c7022d3bc6b73217
Pedro Souza <pedrobr381@hotmail.com>
2020-03-12 17:28:09 -0300

    insert

---
+++ Bd/Inserts.sql
@@ -0,0 +1,15 @@
+USE [MinhaApiCore]
+GO
+
+INSERT INTO [dbo].[EstadoCivil]
+           ([Id]
+           ,[Descricao])
+     VALUES
+           ('6853eb66-06b7-42c3-8988-1bfb2ec1904d', 'Solteiro(a)'),
+		   ('a2e606a8-de35-42e0-862a-de7b7310abdd', 'Casado(a)'),
+		   ('1eddf303-1818-4781-8816-97089ec49c63', 'Divorciado(a)'),
+		   ('ab293056-e9ed-4023-9eff-cafda01a9956', 'Viúvo(a)'),
+		   ('72cfc8c1-a47e-4df8-8382-f70fc1bb7775', 'Separado(a)')
+
+GO
+
